fdaf2e2745640f08cbdb19bb25360564ba998a88
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-11-01 08:08:28 -0500

    Added Form

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,16 +6,34 @@ class MembersController < ApplicationController
 
   end
 
-  def show
 
+def new
+   @members = Member.all
+end
+
+def create
+  m = Member.new
+  m.firstname = params[:firstname]
+  m.secondname = params[:secondname]
+  m.HCP = params[:HCP]
+  m.club = params[:club]
+  m.brand = params[:brand]
+  m.save
+  redirect_to members_url
+end
+
+
+
+
+
+
+  def show
 
     @members = Member.all
 
     member_id = params["id"].to_i
 
-    @player = Member.find_by_id(member_id)
-
-
+    @player = Member.find_by(id: member_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ GolfApp01::Application.routes.draw do
 
 root 'members#index'
 
-get 'details/:id' => 'members#show', as: 'details'
+get '/members' => 'members#index'
+
+get 'members/new' => 'members#new', as: 'new_member'
+post 'members' => 'members#create'
+
+
+
+
+get 'members/:id' => 'members#show', as: 'member'
 
 get 'courses' => 'venues#index', as: 'courses'
 
